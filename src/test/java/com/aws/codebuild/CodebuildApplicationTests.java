aa2ad66ed3b3acc927fe54010a8ff789389d3784
dhana250 <kdhana.2015@gmail.com>
2021-02-14 21:36:03 -0600

    AWS Code Build

---
+++ src/test/java/com/aws/codebuild/CodebuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.codebuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodebuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
